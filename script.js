28811af52bcf70c3a37eb9782332b89a67a8dfbb
John Joshua Ramirez <johnjoshuaramirez.social@gmail.com>
2022-10-31 18:39:28 -0800

    feature: implement add, subtract, multiply, divide and operate functions

--- script.js
+++ script.js
@@ -1,67 +1,70 @@
-const addition = "+";
-const subtraction = "-";
-const multiplication = "*";
-const division = "/";
+const container = document.querySelector(".container");
+const buttons = document.querySelector(".button-group");
+const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+let displayValue = "";
+let firstNum = "";
+let secondNum = "";
+let operator = "";
 
 function add(num1, num2) {
-	return num1 + num2;
+	const result = parseInt(num1) + parseInt(num2);
+	console.log(result);
 }
 
 function subtract(num1, num2) {
-	return num1 - num2;
+	const result = parseInt(num1) - parseInt(num2);
+	console.log(result);
 }
 
 function multiply(num1, num2) {
-	return num1 * num2;
+	const result = parseInt(num1) * parseInt(num2);
+	console.log(result);
 }
 
 function divide(num1, num2) {
-	return num1 / num2;
+	const result = parseInt(num1) / parseInt(num2);
+	console.log(result);
 }
 
 function operate(operator, num1, num2) {
 	if (operator === "+") {
 		add(num1, num2);
+	} else if (operator === "-") {
+		subtract(num1, num2);
+	} else if (operator === "*") {
+		multiply(num1, num2);
+	} else if (operator === "/") {
+		divide(num1, num2);
 	}
 }
 
-const buttons = document.querySelector(".button-group");
-const display = document.querySelector(".display");
-const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operators");
-const displayValue = display.innerText;
+container.addEventListener("click", e => {
+	if (e.target.classList.contains("number")) {
+		display.innerText += e.target.innerText;
+		displayValue += e.target.innerText;
+		console.log(displayValue);
+	}
+});
 
+container.addEventListener("click", e => {
+	if (e.target.classList.contains("operator")) {
+		firstNum = displayValue;
+		operator = e.target.innerText;
+		displayValue = "";
+		display.innerText = "";
+		console.log("firstNum " + firstNum);
+		console.log("secondNum " + secondNum);
+		console.log("operator " + operator);
+	}
+});
 
-buttons.addEventListener("click", e => {
-   console.log(e.target)
-   if (e.target.classList.contains("number")) {
-      display.innerText += e.target.innerText;
-   };
-})
-
-
-
-
-
-
-
-
-
-
-
-// numbers.forEach(number => {
-// 	number.addEventListener("click", () => {
-// 		const numberValue = number.innerText.toString();
-// 		display.innerText += numberValue;
-// 	});
-// });
-
-// operators.forEach(operator => {
-//    operator.addEventListener("click", () => {
-//       if (operator.innerText === "+") {
-//          operate( displayValue)
-//       }
-//    })
-// })
-
-
+container.addEventListener("click", e => {
+	if (e.target.classList.contains("equals")) {
+		secondNum = displayValue;
+      console.log("secondNum " + secondNum);
+		operate(operator, firstNum, secondNum);
+	}
+});
